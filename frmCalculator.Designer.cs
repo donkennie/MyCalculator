19c7fc545af1d3583bbfdc8d2a9d85cd9aaa68b1
Donkennie <ajeigbekehinde160@gmail.com>
2021-11-30 15:52:41 +0100

    feat: created calculator

---
+++ frmCalculator.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace MyCalculator
+{
+    partial class frmCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.txtFirstNumber = new System.Windows.Forms.TextBox();
+            this.txtSecondNumber = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSubstract = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Number:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Second Number:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(71, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Answer:";
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(187, 137);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(53, 13);
+            this.lblAnswer.TabIndex = 3;
+            this.lblAnswer.Text = "b/Answer";
+            // 
+            // txtFirstNumber
+            // 
+            this.txtFirstNumber.Location = new System.Drawing.Point(190, 48);
+            this.txtFirstNumber.Name = "txtFirstNumber";
+            this.txtFirstNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstNumber.TabIndex = 8;
+            // 
+            // txtSecondNumber
+            // 
+            this.txtSecondNumber.Location = new System.Drawing.Point(190, 85);
+            this.txtSecondNumber.Name = "txtSecondNumber";
+            this.txtSecondNumber.Size = new System.Drawing.Size(105, 20);
+            this.txtSecondNumber.TabIndex = 9;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(65, 189);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 10;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSubstract
+            // 
+            this.btnSubstract.Location = new System.Drawing.Point(190, 189);
+            this.btnSubstract.Name = "btnSubstract";
+            this.btnSubstract.Size = new System.Drawing.Size(75, 23);
+            this.btnSubstract.TabIndex = 11;
+            this.btnSubstract.Text = "-";
+            this.btnSubstract.UseVisualStyleBackColor = true;
+            this.btnSubstract.Click += new System.EventHandler(this.btnSubstract_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Location = new System.Drawing.Point(65, 246);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(75, 23);
+            this.btnMultiply.TabIndex = 12;
+            this.btnMultiply.Text = "x";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Location = new System.Drawing.Point(190, 246);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(75, 23);
+            this.btnDivide.TabIndex = 13;
+            this.btnDivide.Text = "/";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // frmCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnSubstract);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtSecondNumber);
+            this.Controls.Add(this.txtFirstNumber);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmCalculator";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.frmCalculator_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.TextBox txtFirstNumber;
+        private System.Windows.Forms.TextBox txtSecondNumber;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSubstract;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnDivide;
+    }
+}
+
---
+++ frmCalculator.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyCalculator
+{
+    public partial class frmCalculator : Form
+    {
+        public frmCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCalculator_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            bool blnFirstNumber;
+            bool blnSecondNumber;
+            string strFirstNumber = txtFirstNumber.Text;
+            string strSecondNumber = txtSecondNumber.Text;
+            blnFirstNumber = CheckIfStringIsValidNumber(strFirstNumber);
+            blnSecondNumber = CheckIfStringIsValidNumber(strSecondNumber);
+            if(blnFirstNumber == true && blnSecondNumber == true)
+            {
+                lblAnswer.Text = (double.Parse(strFirstNumber) + double.Parse(strSecondNumber)).ToString();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a number", "Error");
+            }
+        
+        }
+
+        private void btnSubstract_Click(object sender, EventArgs e)
+        {
+            bool blnFirstNumber;
+            bool blnSecondNumber;
+            string strFirstNumber = txtFirstNumber.Text;
+            string strSecondNumber = txtSecondNumber.Text;
+            blnFirstNumber = CheckIfStringIsValidNumber(strFirstNumber);
+            blnSecondNumber = CheckIfStringIsValidNumber(strSecondNumber);
+            if (blnFirstNumber == true && blnSecondNumber == true)
+            {
+                lblAnswer.Text = (double.Parse(strFirstNumber) - double.Parse(strSecondNumber)).ToString();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a number", "Error");
+            }
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            lblAnswer.Text = (double.Parse(txtFirstNumber.Text) * double.Parse(txtSecondNumber.Text)).ToString();
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            lblAnswer.Text = (double.Parse(txtFirstNumber.Text) / double.Parse(txtSecondNumber.Text)).ToString();
+        }
+
+        private bool CheckIfStringIsValidNumber(string strStringInQuestion)
+        {
+            bool blnFlag = false;
+            foreach (char character in strStringInQuestion)
+            {
+
+                if (character > '0' && character < '9')
+                {
+                    blnFlag = true;
+                }
+
+                else if (txtFirstNumber.Text== "." || txtFirstNumber.Text== "0")
+                {
+                    if (!txtFirstNumber.Text.Contains("."))
+                    {
+                        blnFlag = false;
+                    }
+                    blnFlag =true;
+                    
+                }
+                else if (txtFirstNumber.Text.Contains("-"))
+                {
+                    blnFlag = true;
+                    if (!txtFirstNumber.Text.Contains("-"))
+                    {
+                        blnFlag = false;
+                    }
+                }
+                else
+                {
+                    blnFlag = false;
+                    break;
+                }
+                
+            }
+            return blnFlag;
+        }
+    }
+}
